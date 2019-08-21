a8d2c4b30f42201124f9ca2845b1ccd7c1b5c6b2
mkatenolan <mkatenolan@gmail.com>
2019-08-21 15:23:00 +0100

    set up index.js file in src foler + get server running

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // final app exported to index.js
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const expressHandlebars = require('express-handlebars');
 const controllers = require('./controllers');
 const helpers = require('./views/helpers');
@@ -9,7 +9,7 @@ const helpers = require('./views/helpers');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(controllers);
+// app.use(controllers);
 
 
 module.exports = app;
