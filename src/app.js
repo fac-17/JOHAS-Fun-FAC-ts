702f10983d0344cd725d1d0ed90075f913de3f52
Colette <colette.rs.shaw@gmail.com>
2019-08-21 15:53:41 +0100

    serve main and home templates on app run

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 // final app exported to index.js
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 // const favicon = require('serve-favicon');
-const expressHandlebars = require('express-handlebars');
-const controllers = require('./controllers');
-const helpers = require('./views/helpers');
+const expressHandlebars = require("express-handlebars");
+const controllers = require("./controllers");
+const helpers = require("./views/helpers");
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 // app.use(controllers);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  expressHandlebars({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+    defaultLayout: "main"
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 module.exports = app;
