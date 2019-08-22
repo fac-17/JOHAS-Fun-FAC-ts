d729e45b59284522659a18f3e1145c5285c5390e
Tony Lomax <antl.lomax@gmail.com>
2019-08-22 14:42:29 +0100

    Post data to DB work

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const queries = require('../model/queries/queries')
+const express = require("express");
+const queries = require("../model/queries/queries");
 
-const router = express.Router()
+const router = express.Router();
 
 // const home = require('./home')
 // const makeStatement = require('./makeStatement')
 // const makeGuess = require('./makeGuess')
-const error = require('./error')
-
+const error = require("./error");
 
 router.get("/", (req, res) => {
   res.render("home");
 });
 
 router.get("/makeStatement", (req, res) => {
-  res.render("makeStatement")
+  res.render("makeStatement");
 });
 
 router.post("/submit_statement", (req, res) => {
-// res.render('home')
- console.log('the endpoint is', res.body.url)
+  //res.render("home");
+  console.log("the endpoint is", res);
+  console.log("the req is", req);
 
-  queries.postStatement((err, res) => {
-    if(err){
-      console.log('i am err', err)
-    } else {
-      console.log('i am res', res)
-    }
-  })
+  // queries.postStatement((err, res) => {
+  //   if (err) {
+  //     console.log("i am err", err);
+  //   } else {
+  //     console.log("i am res", res);
+  //   }
+  // });
 });
 
-router.use(error.client)
-router.use(error.server)
+router.use(error.client);
+router.use(error.server);
 
 module.exports = router;
