84ace45c2c94cabd281482bac357d0f00f5f17e4
mkatenolan <mkatenolan@gmail.com>
2019-08-22 10:15:12 +0100

    Debug database files to setup local database

--- src/model/database/connection.js
+++ src/model/database/connection.js
@@ -1,13 +1,18 @@
 const { Pool } = require('pg');
 const url = require('url');
+let DATABASE_URL = process.env.DATABASE_URL;
 
 if(process.env.NODE_ENV !== "production"){
   require("dotenv").config();
 };
 
+if(process.env.NODE_ENV === "test") {
+  DATABASE_URL = process.env.TEST_URL;
+};
+
 if(!process.env.DATABASE_URL){
   throw new Error("Environment variable DATABASE_URL must be set");
-}
+};
 
 const params = url.parse(process.env.DATABASE_URL);
 
@@ -17,7 +22,7 @@ const options = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECION || 2
+  max: process.env.DB_MAX_CONNECION || 2,
   user: username,
   password,
   ssl: params.hostname !== 'localhost'
