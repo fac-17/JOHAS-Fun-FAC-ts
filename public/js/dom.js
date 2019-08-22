b4fbbc9e5ce35375208259749013938e32ebe776
Tony Lomax <antl.lomax@gmail.com>
2019-08-22 17:24:50 +0100

    point tracker finished

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,9 +1,30 @@
-console.log("this is the js");
-
 const answers = Array.from(document.querySelectorAll(".answer"));
 const trueButtons = Array.from(document.querySelectorAll(".button-true"));
-const falseButtons = Array.from(document.querySelectorAll(".button-true"));
+const falseButtons = Array.from(document.querySelectorAll(".button-false"));
+let scoreContainer = document.querySelector(".score");
+let count = 0;
 
 answers.forEach(answer => {
   answer.style.display = "none";
+});
+
+trueButtons.forEach((btn, ind) => {
+  btn.addEventListener("click", () => {
+    answers[ind].style.display = "block";
+    if (answers[ind].innerText === "true") {
+      count += 1;
+      scoreContainer.textContent = `Score= ${count}`;
+    }
+  });
+});
+
+falseButtons.forEach((btn, ind) => {
+  btn.addEventListener("click", () => {
+    console.log(ind);
+    answers[ind].style.display = "block";
+    if (answers[ind].innerText !== "true") {
+      count += 1;
+      scoreContainer.textContent = `Score= ${count}`;
+    }
+  });
 });
