3c1fe685c78851358bd9beea8043c3e33d4c7c1e
mkatenolan <mkatenolan@gmail.com>
2019-08-22 11:17:10 +0100

    Add test for get data query

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -1,8 +1,22 @@
 const test = require("tape");
-const buildScript = require("../model/database/build");
+const buildDatabase = require("../model/database/build");
 const queries = require("../model/queries/queries");
 
 test("Tape is working", t => {
   t.equal(1, 1, "1 equals 1");
   t.end();
 });
+
+test("Test that getData function returns info", t => {
+  buildDatabase((err, res) => {
+    t.error(err, 'No error');
+
+    let expected = { id:1, author: 'sarah', statement:'I had a sixth finger', answer:false};
+
+    queries.getData((err, result) => {
+      if(err) console.log(err);
+      t.deepEqual(result, expected, "returns info from database");
+      t.end();
+    })
+  })
+})
