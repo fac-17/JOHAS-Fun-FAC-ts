8f37721a124c748a77e5ce42c53a4b60659da2db
Tony Lomax <antl.lomax@gmail.com>
2019-08-22 11:01:48 +0100

    Tests set up

--- src/model/database/build.js
+++ src/model/database/build.js
@@ -1,21 +1,16 @@
-const fs = require('fs');
-const connection = require('./connection');
+const fs = require("fs");
+const connection = require("./connection");
 const sql = fs.readFileSync(`${__dirname}/build.sql`).toString();
 
 const buildDatabase = () => {
-  // console.log("hellooo");
   connection.query(sql, (err, res) => {
-   if(err) {
-     console.log(err, 'error')
-  } else {
-   // console.log('Database created')
-   //  connection.end(() => {
-   //    console.log('Connection closed')
-   //   })
-   console.log(res);
+    if (err) {
+      console.log(err, "error");
+    } else {
+      //console.log(res.message);
     }
-   })
-}
+  });
+};
 
 buildDatabase();
 
---
+++ src/tests/db.test.js
@@ -0,0 +1,8 @@
+const test = require("tape");
+const buildScript = require("../model/database/build");
+const queries = require("../model/queries/queries");
+
+test("Tape is working", t => {
+  t.equal(1, 1, "1 equals 1");
+  t.end();
+});
