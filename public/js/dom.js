9ba77c9132749f6f555e1f3f5587202e271c9ff4
Tony Lomax <antl.lomax@gmail.com>
2019-08-23 12:31:33 +0100

    Fixes Fixed spam score Capitlised answer Hid buttons on button click Fix #31

--- public/js/dom.js
+++ public/js/dom.js
@@ -8,23 +8,76 @@ answers.forEach(answer => {
   answer.style.display = "none";
 });
 
-trueButtons.forEach((btn, ind) => {
-  btn.addEventListener("click", () => {
-    answers[ind].style.display = "block";
-    if (answers[ind].innerText === "true") {
-      count += 1;
-      scoreContainer.textContent = `Score= ${count}`;
-    }
-  });
-});
+answerFunction = buttonsArray => {
+  buttonsArray.forEach((btn, ind) => {
+    btn.addEventListener("click", () => {
+      let answerCapital =
+        answers[ind].innerText.charAt(0).toUpperCase() +
+        answers[ind].innerText.slice(1);
 
-falseButtons.forEach((btn, ind) => {
-  btn.addEventListener("click", () => {
-    console.log(ind);
-    answers[ind].style.display = "block";
-    if (answers[ind].innerText !== "true") {
-      count += 1;
-      scoreContainer.textContent = `Score= ${count}`;
-    }
+      answers[ind].style.display = "block";
+      answers[ind].innerText = `Answer is: ${answerCapital}`;
+      if (btn.textContent === "True" && answers[ind].innerText === "true") {
+        count += 1;
+        scoreContainer.textContent = `Score= ${count}`;
+        btn.style.display = "none";
+        falseButtons[ind].style.display = "none";
+      } else if (
+        btn.textContent === "False" &&
+        answers[ind].textContent === "false"
+      ) {
+        count += 1;
+        scoreContainer.textContent = `Score= ${count}`;
+        btn.style.display = "none";
+        trueButtons[ind].style.display = "none";
+      } else {
+        // btn.style.display = "none";
+        falseButtons[ind].style.display = "none";
+        trueButtons[ind].style.display = "none";
+      }
+    });
   });
-});
+};
+
+answerFunction(trueButtons);
+answerFunction(falseButtons);
+
+// trueButtons.forEach((btn, ind) => {
+//   btn.addEventListener("click", () => {
+//     let answerCapital =
+//       answers[ind].innerText.charAt(0).toUpperCase() +
+//       answers[ind].innerText.slice(1);
+
+//     answers[ind].style.display = "block";
+//     answers[ind].innerText = `Answer is: ${answerCapital}`;
+//     if (answers[ind].innerText === "true") {
+//       count += 1;
+//       scoreContainer.textContent = `Score= ${count}`;
+//       btn.style.display = "none";
+//       falseButtons[ind].style.display = "none";
+//     } else {
+//       btn.style.display = "none";
+//       falseButtons[ind].style.display = "none";
+//     }
+//   });
+// });
+
+// falseButtons.forEach((btn, ind) => {
+//   btn.addEventListener("click", () => {
+//     let answerCapital =
+//       answers[ind].innerText.charAt(0).toUpperCase() +
+//       answers[ind].innerText.slice(1);
+
+//     answers[ind].style.display = "block";
+//     answers[ind].innerText = `Answer is: ${answerCapital}`;
+//     if (answers[ind].innerText !== "true") {
+//       count += 1;
+//       scoreContainer.textContent = `Score= ${count}`;
+//       btn.style.display = "none";
+//       trueButtons[ind].style.display = "none";
+//     } else {
+//       btn.style.display = "none";
+//       trueButtons[ind].style.display = "none";
+//     }
+//   });
+// });
